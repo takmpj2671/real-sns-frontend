6f6d8a5180517836ef4de86270db3b5891949dcc
T.Yusuke <taklahashi0711@outlook.jp>
2025-09-07 03:34:15 +0900

    Fix: Update Vite proxy config to target localhost:5001

--- src/components/timeline/Timeline.jsx
+++ src/components/timeline/Timeline.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Share from "../share/Share.jsx";
 import "./Timeline.css";
 import Post from "../post/Post.jsx";
-import { Posts } from "../../../dummyData.js";
-// import { useState } from "react";
+import axios from "axios";
+// import { Posts } from "../../../dummyData.js";
 
 export default function Timeline() {
-  // const [val, setVal] = useState();
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // console.log("useEffect実行");
+    const fetchPosts = async() => {
+      const response = await axios.get("/api/posts/timeline/6860feec3e2aa3f2dcb4dc91");
+      console.log(response);
+    };
+    fetchPosts();
+  }, []); //ページのマウント時に一回だけここに書かれるものが読み込まれる。
+
   return (
     <div className="timeline">
       <div className="timelineWrapper">
@@ -15,9 +25,9 @@ export default function Timeline() {
 {/*アロー関数での注意点。
   一行: () => <div>Hello</div>
   複数行: () => (JSX) または () => { return JSX; } */}
-        {Posts.map((post) => (
+        {/* {Posts.map((post) => (
           <Post post={post} key={post.id}/>
-        ))}
+        ))} */}
       </div>
     </div>
   );
